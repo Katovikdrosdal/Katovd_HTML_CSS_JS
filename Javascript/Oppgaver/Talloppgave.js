262b510245ed494b31b62ed421842e3b008476ba
Kato <Kato.v.d@hotmail.com>
2024-10-17 11:02:13 +0200

    h

---
+++ Javascript/Oppgaver/Talloppgave.js
@@ -0,0 +1,20 @@
+function Calculate() {
+    let Number = document.getElementById("Number").value;
+    
+    let Half = Number / 2;
+    let Double = Number * 2;
+    let Triple = Number * 3;
+    let Squared = Number * Number;
+
+    let Answer = `Number is ${Number}.
+    <br/>
+    Half is ${Half}, 
+    <br/>
+    Double is ${Double},
+    <br/> 
+    Triple is ${Triple},
+    <br/> 
+    Squared is ${Squared}.`;
+
+    document.getElementById("output").innerHTML = Answer;
+}
