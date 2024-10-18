f7b5dfabca0aab36711a5fb0faf88d614b8f07f3
Kato <Kato.v.d@hotmail.com>
2024-10-18 10:00:28 +0200

    Added CSS. Polished script and HTML

--- Javascript/Oppgaver/Talloppgave.js
+++ Javascript/Oppgaver/Talloppgave.js
@@ -1,6 +1,13 @@
 function Calculate() {
     let Number = document.getElementById("Number").value;
-    
+
+    if (Number === "") {
+        document.getElementById("output").innerHTML = "Please enter a valid number.";
+        return;
+    }
+
+    Number = parseFloat(Number);
+
     let Half = Number / 2;
     let Double = Number * 2;
     let Triple = Number * 3;
@@ -8,12 +15,12 @@ function Calculate() {
 
     let Answer = `Number is ${Number}.
     <br/>
-    Half is ${Half}, 
+    Half is ${Half},
     <br/>
     Double is ${Double},
-    <br/> 
+    <br/>
     Triple is ${Triple},
-    <br/> 
+    <br/>
     Squared is ${Squared}.`;
 
     document.getElementById("output").innerHTML = Answer;
